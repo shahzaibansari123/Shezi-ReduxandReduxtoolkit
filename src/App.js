644ea1a68c347bd9175646cff23ac3e6782ee3fe
Shahzaib Ansari <61605534+shahzaibansari123@users.noreply.github.com>
2023-03-17 23:45:54 +0500

    Minor changes

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import logo from './logo.svg';
 import './App.css';
 import Home from './Home';
-import {Provider} from 'react-redux'
+import {Provider} from 'react-redux';
+import store from './store'
 
 function App() {
 
   return (
     <div className="App">
-<Provider >
+<Provider store={store}>
       <Home />
 </Provider>
     </div>
