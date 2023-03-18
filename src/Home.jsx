f1687e43510667941d342f845725e4eacc308c59
shahzaibansari123 <ansarishahzaib567@gmail.com>
2023-03-18 22:47:35 +0500

    working on it

--- src/Home.jsx
+++ src/Home.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 import {useDispatch} from'@reduxjs/toolkit'
 
 const Home = (value) => {
+const dispatch= useDispatch();
 const value=20;
+
+
   return (
     <div>
 <h1>{value}</h1>
